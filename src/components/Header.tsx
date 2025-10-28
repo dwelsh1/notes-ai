df8d583141bf725f8bf67bfa38930b5e1e794a6a
Don Welsh <don@vbreview.com>
2025-10-27 22:16:10 -0700

    style: prettier fix for Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Settings, Square, Languages, CheckCircle, FileText, Lightbulb } from 'lucide-react';
+import {
+  Settings,
+  Square,
+  Languages,
+  CheckCircle,
+  FileText,
+  Lightbulb,
+} from 'lucide-react';
 
 interface HeaderProps {
   onTranslate: () => void;
